7a6f5e63c6effdd419179290f70590712cf9c493
aaa <aaa@domain.pl>
2022-10-25 16:31:33 +0200

    Lab 5

---
+++ lab_3/zad_1.py
@@ -0,0 +1,7 @@
+
+def concatenate_names(name: str, surname: str) -> str:
+    return f'Cześć {name} {surname}!'
+
+
+fullname = concatenate_names("Szymon", "Kania")
+print(fullname)
---
+++ lab_3/zad_2.py
@@ -0,0 +1,6 @@
+
+def multiply(multiplicand: int, multiplier: int) -> int:
+    return multiplicand * multiplier
+
+
+print(multiply(10, 5))
